bfe061d098fb999c141312cffe1aabe1dc39fd00
BenjaminWorsley <benworsley00@gmail.com>
2025-08-25 09:14:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,7 @@ if ingredient_list:
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader("Nuttritional Information" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
